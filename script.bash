9b0bdf2ebe8b538e855d95f3cac80962453ad0cd
kustyk97 <67096796+kustyk97@users.noreply.github.com>
2024-12-28 18:39:41 +0100

    add script for testing

---
+++ script.bash
@@ -0,0 +1,6 @@
+python train.py --batch_size 1
+python train.py --batch_size 2
+python train.py --batch_size 4
+python train.py --batch_size 32
+python train.py --batch_size 64
+python train.py --batch_size 128
